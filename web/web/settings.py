992594104666e9968c5910f85f509d30c3b0bce4
KIM MINJEONG <mocaron00@ewhain.net>
2022-01-13 01:10:23 +0900

    db

--- web/web/settings.py
+++ web/web/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Project',
+        'USER': 'root',
+        'PASSWORD': 'tkfkddl12',
+        'HOST': 'localhost',
+        'PORT': '3306'
+
     }
 }
 
