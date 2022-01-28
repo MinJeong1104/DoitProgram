1a9fcedbd9493a9eec2dfd7acbe637bc9d41e037
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2022-01-29 02:43:27 +0900

    Update main.py

--- web/scripts/main.py
+++ web/scripts/main.py
@@ -285,7 +285,7 @@ def run():
                                   tabula.convert_into(file, dst + "/p1("+count+").csv", pages='all', output_format="csv",
                                                       stream=True, lattice=False)
                           if (fileEx=="pdf"):
-                              tbpath=downloadPath+"\\"+new_filename+number+"."+fileEx
+                              tbpath=os.path.join(downloadPath,new_filename+number+"."+fileEx)
                               df = tabula.read_pdf(tbpath, pages='all', stream=True, lattice=False,encoding='Windows 1252')
                               pdf_extract_info(tbpath)
                           Class(number=classNum, title=className,subnum=number, professor=professor, downloadPath=downloadPath, filename=new_filename, crawled_time=crawled_time).save()
