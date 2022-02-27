b9cfd237037ed4e4dfdbcdd2a741f043330bf9ca
KIM MINJEONG <mocaron00@ewhain.net>
2022-02-27 22:57:25 +0900

    NLP Program 적용

--- web/scripts/main.py
+++ web/scripts/main.py
@@ -297,9 +297,9 @@ def run():
                               df = tabula.read_pdf(tbpath, pages='all', stream=True, lattice=False,encoding='utf-8')
                               pdf_extract_info(tbpath) """
                           tbpath = os.path.join(downloadPath, new_filename + number + "." + fileEx)
-                          if(fileEx=="pdf"):
-                          #pdf_file = moveDir(tbpath)
-                              pdf_file=pdf_extract_table_info(tbpath)
+                          if(fileEx!="hwp"):
+                              pdf_file = moveDir(tbpath)
+                              pdf_extract_table_info(pdf_file)
                               merged_img = img_merge(pdf_to_jpg(pdf_file))
                               word_list = extract_txt_from_img(merged_img)
                               print(word_list)
