35f610ab28b1895fd5b8d4797bef4232f633ffc6
DESKTOP-FLCJD0T\MinJeong <mocaron00@ewhain.net>
2022-04-01 14:28:11 +0900

    hwp read

--- web/scripts/main.py
+++ web/scripts/main.py
@@ -50,14 +50,14 @@ def run():
     # 각 Section 내 text 추출
     section_text = ""
     i = 0
-    size = len(unpacked_data)
+    size = len(data)
     while i < size:
-      header = struct.unpack_from("<I", unpacked_data, i)[0]
+      header = struct.unpack_from("<I", data, i)[0]
       rec_type = header & 0x3ff
       rec_len = (header >> 20) & 0xfff
 
       if rec_type in [67]:
-        rec_data = unpacked_data[i + 4:i + 4 + rec_len]
+        rec_data = data[i + 4:i + 4 + rec_len]
         section_text += rec_data.decode('utf-16')
         section_text += "\n"
 
