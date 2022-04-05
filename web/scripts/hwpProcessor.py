58b786901eb7c89a9bcafbf4ccda9336dfa263a1
KIM MINJEONG <mocaron00@ewhain.net>
2022-04-06 03:34:01 +0900

    test

--- web/scripts/hwpProcessor.py
+++ web/scripts/hwpProcessor.py
@@ -42,7 +42,7 @@ def get_hwp_text(f):
             print("rec_len"+str(rec_len))
             rec_data = data[i + 4:i + 4 + rec_len]
             print("rec_data"+str(rec_data))
-            section_text += rec_data.decode('UTF-16', errors='ignore')
+            section_text += rec_data.decode()
             section_text += "\n"
             print("section_text"+str(section_text))
             i += 4 + rec_len
