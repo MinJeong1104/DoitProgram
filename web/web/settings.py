9ff2ec6b74005af70a8703c77e6444150a7e2741
DESKTOP-FLCJD0T\MinJeong <mocaron00@ewhain.net>
2022-05-06 16:22:24 +0900

    firebaseauth

--- web/web/settings.py
+++ web/web/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
-        "config.authentication.FirebaseAuthentication",
+        "config.authentication.FirebaseAuthentication"
     ),
 }
 
