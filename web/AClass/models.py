da2342ff3dbf35f7bb950c384e41f6dcd2fa28a9
KIM MINJEONG <mocaron00@ewhain.net>
2022-06-05 08:46:07 +0900

    test

--- web/AClass/models.py
+++ web/AClass/models.py
@@ -15,8 +15,8 @@ class Class(models.Model):
     crawled_time = models.DateTimeField(auto_now=True,null=True)
 
 class Todo(models.Model):
-    idnums =models.CharField(max_length=200, null=False, default='')
-    days = models.CharField(max_length=50,null=True,default='',primary_key=True)
+    idnums =models.CharField(max_length=200, null=True, default='')
+    days = models.CharField(max_length=50,null=False,default='',primary_key=True)
     activities = models.CharField(max_length=50,null=True,default='')
     """idnum = models.ForeignKey(Class, on_delete=models.CASCADE)"""
     weeks = models.CharField(max_length=50, null=True, default='')
