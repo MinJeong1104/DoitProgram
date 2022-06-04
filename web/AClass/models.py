c8dbcb43eb19e269d6016e1e6cc2b9de49378ee5
KIM MINJEONG <mocaron00@ewhain.net>
2022-06-05 07:18:51 +0900

    test

--- web/AClass/models.py
+++ web/AClass/models.py
@@ -17,7 +17,8 @@ class Class(models.Model):
 class Todo(models.Model):
     days = models.CharField(max_length=50,null=True,default='')
     activities = models.CharField(max_length=50,null=True,default='')
-    idnum = (max_length=200,null=False,default='',primary_key=True)
+    """idnum = models.ForeignKey(Class, on_delete=models.CASCADE)"""
+    idnum - models.CharField(max_length=200,null=False,default='',primary_key=True)
     weeks = models.CharField(max_length=50, null=True, default='')
     Ranges = models.CharField(max_length=50,null=True,default='')
     Materials = models.CharField(max_length=50,null=True,default='')
