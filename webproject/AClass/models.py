cca660de6da1b76f7ef8a0d299ffcb29eb44faf7
KIM MINJEONG <mocaron00@ewhain.net>
2022-06-11 04:39:17 +0900

    todo model 수정

--- webproject/AClass/models.py
+++ webproject/AClass/models.py
@@ -18,7 +18,7 @@ class Class(models.Model):
 
 class Todo(models.Model):
     idnums = models.CharField(max_length=200, null=True, default='')
-    days = models.CharField(max_length=50,null=False,default='', primary_key=True)
+    days = models.CharField(max_length=50,null=False,default='')
     activities = models.CharField(max_length=50,null=True,default='')
     weeks = models.CharField(max_length=50, null=True, default='')
     Ranges = models.CharField(max_length=50,null=True,default='')
